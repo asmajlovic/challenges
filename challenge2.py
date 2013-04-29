34e8de7d3474dcb9849f7eaf59e87fb9fc3ebe06
asmajlovic <a.smajlovic@gmail.com>
2013-04-29 10:27:48 +0000

    Some refactoring, adding argparse and image cleanup (we are cloning after all)

--- challenge2.py
+++ challenge2.py
@@ -14,128 +14,141 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import os
 import sys
+import novaclient.exceptions
 from math import ceil
 from time import sleep
 
 import pyrax
 from pyrax import exceptions as e
 
-# Set image progress toolbar width
-toolbar_width = 50
+# Set progress toolbar width (used with image and server creation progress)
+TOOLBAR_WIDTH = 50
 
-# Define the authentication credentials file location and request that pyrax
-# makes use of it. If not found, let the client/user know about it.
+# Location of pyrax configuration file
+CONFIG_FILE = "~/.rackspace_cloud_credentials"
 
-# Use a credential file in the following format:
-# [rackspace_cloud]
-# username = myusername
-# api_key = 01234567890abcdef
-# region = ORD
+def main():
+    """
+    Challenge 2
+    -- Write a script that clones a server (takes an image and deploys the
+       image as a new server)
+    """
 
-try:
-    creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
-    pyrax.set_credential_file(creds_file, "ORD")
-except e.AuthenticationFailed:
-    print ("ERROR: Authentication failed. Please check and confirm "
-           "that the API username, key, and region are in place and correct.")
-    sys.exit(1)
-except e.FileNotFound:
-    print "ERROR: Credentials file '%s' not found" % (creds_file)
-    sys.exit(1)
+    parser = argparse.ArgumentParser(description=("Cloud Server cloning "
+                                                  "application"))
+    parser.add_argument("-s", "--server", action="store", required=True,
+                        metavar="SERVER_ID", type=str,
+                        help=("ID of server to be cloned"))
+    parser.add_argument("-n", "--name", action="store", required=False,
+                        metavar="CLONE_NAME", type=str,
+                        help=("Name of cloned server (default appends "
+                              "'-copy' to original server name"))
+    parser.add_argument("-r", "--region", action="store", required=False,
+                        metavar="REGION", type=str,
+                        help=("Region where servers should be built (defaults"
+                              " to 'ORD'"), choices=["ORD", "DFW", "LON"],
+                              default="ORD")
 
-# Use a shorter Cloud Servers class reference string
-# This simplifies invocation later on (less typing)
-cs = pyrax.cloudservers
+    # Parse arguments (validate user input)
+    args = parser.parse_args()
+    
+    # Define the authentication credentials file location and request that
+    # pyrax makes use of it. If not found, let the client/user know about it.
 
-# Check and confirm the account in question has servers at the moment,
-# otherwise exit gracefully
-servers = cs.servers.list()
-if len(servers) == 0:
-    print ("You do not appear to have any servers at the moment.\n"
-           "Please create one manually then attempt to clone again.")
-    sys.exit(2)
+    # Use a credential file in the following format:
+    # [rackspace_cloud]
+    # username = myusername
+    # api_key = 01234567890abcdef
+    # region = LON
 
-# List the servers currently under the account
-#    NOTE: May need to review this approach given there may be a significant
-#          number of servers under the account already
-srv_id_dict = {}
-srv_flavor_dict = {}
-print "Select a server from which an image will be created:"
-for pos, srv in enumerate(servers):
-    print "%2d) %s [%s]" % (pos, srv.name, srv.accessIPv4)
-    srv_id_dict[str(pos)] = srv.id
-    srv_flavor_dict[str(pos)] = srv.flavor["id"]
+    try:
+        creds_file = os.path.expanduser(CONFIG_FILE)
+        pyrax.set_credential_file(creds_file, args.region)
+    except e.AuthenticationFailed:
+        print ("ERROR: Authentication failed. Please check and confirm "
+               "that the API username, key, and region are in place and "
+               "correct.")
+        sys.exit(1)
+    except e.FileNotFound:
+        print "ERROR: Credentials file '%s' not found" % (creds_file)
+        sys.exit(2)
 
-# Ask the user to pick the server to clone
-selection = None
-while selection not in srv_id_dict:
-    if selection is not None:
-        print "   -- Invalid choice"
-    selection = raw_input("Select a server to clone: ")
+    # Use a shorter Cloud Servers class reference string
+    # This simplifies invocation later on (less typing)
+    cs = pyrax.cloudservers
 
-# We know the ID and flavour, now we need a name for the image
-server_id = srv_id_dict[selection]
-flavor_id = srv_flavor_dict[selection]
-iname = raw_input("Enter a name for the image: ")
-sname = raw_input("Enter a name for the new server to be created: ")
+    # Attempt to locate the server using the ID provided
+    try:
+        src_srv = cs.servers.get(args.server)
+    except:
+        print ("ERROR: Could not find server with ID '%s'\nPlease check "
+               "and try again" % (args.server))
+        sys.exit(3)    
 
-# Kick off the image build
-img_id = cs.servers.create_image(server_id, iname)
-print "Image '%s' with ID '%s' is being created." % (iname, img_id)
+    if args.name:
+        dst_srv_name = args.name
+    else:
+        dst_srv_name = src_srv.name = "-copy"
 
-# Prepare progress toolbar
-sys.stdout.write("[%s]" % (" " * toolbar_width))
-sys.stdout.flush()
-sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    # Attempt to kick off the image build, freak out if it's already in
+    # progress
+    try:
+        img_id = cs.servers.create_image(src_srv.id, dst_srv_name)
+        print "Server image creation in progress..."
+    except novaclient.exceptions.ClientException as err:
+        print "ERROR: Image creation request failed\n%s" % (err)
+        sys.exit(4)
 
-# Grab the image details
-img = cs.images.get(img_id)
-
-# Show image creation progress while the status is 'SAVING'
-while img.status in ["SAVING"]:
-    progress = int(ceil(img.progress / 2))
-    sys.stdout.write("%s" % ("=" * progress))
-    sys.stdout.flush()
-    sys.stdout.write("\b" * (progress)) # Reset progress bar
-    sleep(5)
     img = cs.images.get(img_id)
-print
 
-# Something is not right with the image creation, bail out gracefully
-if img.status not in ["ACTIVE"]:
-    print ("Something went wrong during the image creation\nPlease review the"
-           "output below:\n\nID: %s\nName: %s\nStatus: %s\n\n" %
-           (img.id, img.name, img.status))
-    sys.exit(3)
-else:
-    print "Image creation complete.  Building server..."
+    # Show image creation progressing while the status is 'SAVING'
+    while img.status in ["SAVING"]:
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        sleep(15)
+        img = cs.images.get(img_id)
+    print
 
-# All is well with the new image, create a new server using it as a template
-srv = cs.servers.create(sname, img_id, flavor_id)
+    # Something is not right with the image creation, bail out gracefully
+    if img.status not in ["ACTIVE"]:
+        print ("ERROR: Something went wrong during the image creation"
+               "Status: %s\n\n"% (img.status))
+        sys.exit(5)
+    else:
+        print "Image creation complete.  Building server..."
 
-# Let's do the progress thing all over again, this time for the server build
-sys.stdout.write("[%s]" % (" " * toolbar_width))
-sys.stdout.flush()
-sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+    # All is well with the new image, create a new server using it
+    # as a template
+    srv = cs.servers.create(dst_srv_name, img.id, src_srv.flavor["id"])
 
-while srv.status in ["BUILD"]:
-    progress = int(ceil(srv.progress / 2))
-    sys.stdout.write("%s" % ("=" * progress))
-    sys.stdout.flush()
-    sys.stdout.write("\b" * (progress)) # Reset progress bar
-    sleep(5)
-    srv = cs.servers.get(srv.id)
-print
+    while srv.status in ["BUILD"]:
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        sleep(15)
+        srv.get()  # Update server details
+    print
 
-# Server build has issues, show the status
-if srv.status not in ["ACTIVE"]:
-    print ("Something went wrong during the server creation\nPlease review the"
-           "output below:\n\nID: %s\nName: %s\nStatus: %s\n\n" %
-           (srv.id, srv.name, srv.status))
-    sys.exit(4)
-# All is well, and we're done
-else:
-    print ("Cloning completed successfully\nServer name: %s\nNetworks: %s" %
-          (srv.name, srv.networks))
+    # Server build has issues, show the status
+    if srv.status not in ["ACTIVE"]:
+        print ("Something went wrong during the server creation\nPlease "
+               "review the output below:\n\nID: %s\nName: %s\nStatus: %s\n\n"
+               % (srv.id, srv.name, srv.status))
+        sys.exit(6)
+    # All is well
+    else:
+        print ("Cloning completed successfully\nServer details:\n\tName: %s"
+               "\n\tAdmin password: %s\n\tNetworks:\n\t\tPublic #1: %s\n\t\t"
+                       "Public #2: %s\n\t\tPrivate: %s"
+                       % (srv.name, srv.adminPass, srv.networks["public"][0],
+                          srv.networks["public"][1],
+                          srv.networks["private"][0]))
+                          
+    # Clean up the image and we're done
+    cs.images.delete(img_id)
+
+
+if __name__ == '__main__':
+    main()
