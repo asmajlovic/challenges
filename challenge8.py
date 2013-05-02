6ceb28daa8f1a7a0f5f23228ebfb4676f58046a1
asmajlovic <a.smajlovic@gmail.com>
2013-05-02 03:05:32 +0000

    Better DNS zone and existing container checks - some additional code refactoring

--- challenge8.py
+++ challenge8.py
@@ -23,222 +23,244 @@ from time import sleep
 import pyrax
 from pyrax import exceptions as e
 
-# Set upload progress toolbar width
-toolbar_width = 50
+# Set progress toolbar width (used with image and server creation progress)
+TOOLBAR_WIDTH = 50
 
-# Function to return current zones under the account in question
+# Location of pyrax configuration file
+CONFIG_FILE = "~/.rackspace_cloud_credentials"
+
+# Minimum TTL (in seconds) for a CDN enabled container
+MIN_TTL = 900
+
+# Default TTL (in seconds) for a Cloud DNS record
+DEFAULT_TTL = 300
+
 def zone_list(obj):
-    # Use a paginated request format, in case there is a significant zone count
-    page_size = 25
-    count = 0
+    """
+    Return all zones under an account
+    """
+    # Get and return the zone list
+    doms = obj.list()
+    return doms
 
-    # Determine if any zones actually exist under the account, if not, request
-    # that one be created
-    doms = obj.list(limit=page_size)
-    count += len(doms)
 
-    # Loop until all zones have been considered
-    while True:
-        try:
-            doms = obj.list_next_page()
-            count += len(doms)
-        except e.NoMoreResults:
-            break
+def main():
+    """
+    Challenge 8
+    -- Write a script that will create a static webpage served out of
+       Cloud Files. The script must create a new container, CDN enable it,
+       enable it to serve an index file, create an index file object,
+       upload the object to the container, and create a CNAME record
+       pointing to the CDN URL of the container.
+    """
+    # Parse script parameters
+    parser = argparse.ArgumentParser(description=("Create a Cloud Files "
+                                                  "static web site from a "
+                                                  "local folder/directory"))
+    parser.add_argument("-d", "--directory", action="store",
+                        required=True, type=str, metavar="SRC_DIRECTORY",
+                        help="Local directory to upload content from")
+    parser.add_argument("-c", "--container", action="store",
+                        required=True, type=str, metavar="CONTAINER_NAME",
+                        help=("Container name where content should be "
+                              "uploaded"))
+    parser.add_argument("-q", "--fqdn", action="store",
+                        required=True, type=str, metavar="CDN_URL_CNAME",
+                        help="Fully qualified domain name for CDN CNAME")
+    parser.add_argument("-r", "--region", action="store", required=False,
+                            metavar="REGION", type=str,
+                            help=("Region where container should be created"
+                                  " (defaults to 'ORD'"),
+                                  choices=["ORD", "DFW", "LON"],
+                                  default="ORD")
+    parser.add_argument("-t", "--cdn-ttl", action="store",
+                        required=False, metavar="CDN_TTL",
+                        type=int, help=(("CDN TTL (in seconds) for the "
+                              "container (default %d seconds)") % (MIN_TTL)),
+                               default=MIN_TTL)
+    parser.add_argument("-i", "--index", action="store",
+                        required=False, metavar="STATIC_WEB_INDEX",
+                        type=str, help=("Static web index file (default "
+                                  "'index.html')"), default="index.html")
+    parser.add_argument("-l", "--cname-ttl", action="store",
+                        required=False, type=int, metavar="CNAME_TTL",
+                        help=(("CNAME record TTL (in seconds) for the CDN "
+                              "URI (default %d seconds)") % (DEFAULT_TTL)),
+                               default=DEFAULT_TTL)
+    parser.add_argument("-f", "--force", action="store_true",
+                        required=False, help=("Permit upload to an "
+                        "existing container"))
 
-    # Return the domains and count
-    return (doms, count)
+    # Parse arguments (validate user input)
+    args = parser.parse_args()
 
-# We may be asking for a TTL a few times, here's the function to handle it
-def get_ttl(rec_type):
-    ttl = None
-    
-    while not ttl:
-        try:
-            ttl = int(raw_input("Please enter the %s record TTL (in seconds): "
-                                 % (rec_type)))
-            # User could potentially provide a negative integer, check to make
-            # sure TTL is at least 300 seconds
-            if ttl < 300:
-                print "Invalid selection. TTL must be at least 300s (5 min)"
-                ttl = None
-        except ValueError:
-            print "TTL must be a positive integer >300, please try again."
-    return ttl
-
-# Parse script parameters
-parser = argparse.ArgumentParser(description=("Create a Cloud Files static "
-                                              "web site from a local "
-                                              "folder/directory"))
-parser.add_argument("-d", "--directory", action="store",
-                    required=True, type=str,
-                    help="Local directory to upload content from")
-parser.add_argument("-c", "--container", action="store",
-                    required=True, type=str,
-                    help="Container name where content should be uploaded")
-parser.add_argument("-t", "--ttl", action="store", required=False, type=int,
-                    help=("CDN TTL (in seconds) for the container "
-                          "(default 900 seconds)"), default=900)
-parser.add_argument("-i", "--index", action="store", required=False, type=str,
-                    help="Static web index file (default 'index.html')",
-                    default="index.html")
-
-args = parser.parse_args()
-
-# Determine if the upload directory exists
-if not os.path.isdir(args.directory):
-    print ("ERROR: Specified directory (%s) does not exist, please check "
-           "the path and try again)" % (args.directory))
-    sys.exit(1)
-
-# Define the authentication credentials file location and request that pyrax
-# makes use of it. If not found, let the client/user know about it.
-
-# Use a credential file in the following format:
-# [rackspace_cloud]
-# username = myusername
-# api_key = 01234567890abcdef
-# region = ORD
-
-try:
-    creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
-    pyrax.set_credential_file(creds_file, "ORD")
-except e.AuthenticationFailed:
-    print ("ERROR: Authentication failed. Please check and confirm "
-           "that the API username, key, and region are in place and correct.")
-    sys.exit(1)
-except e.FileNotFound:
-    print "ERROR: Credentials file '%s' not found" % (creds_file)
-    sys.exit(1)
-
-# Use a shorter Cloud Files and DNS class reference strings
-# This simplifies invocation later on (less typing)
-cf = pyrax.cloudfiles
-dns = pyrax.cloud_dns
-
-# Determine if container already exists, otherwise create it
-try:
-    print "Checking if container exists..."
-    cont = cf.get_container(args.container)
-except:
-    cont = None
-
-# Container not found, create it and CDN enable
-if cont is None:
-    try:
-        print ("Container '%s' does not exist, creating"
-               % (args.container))
-        cont = cf.create_container(args.container)
-        print "Setting CDN TTL to %s seconds" % (args.ttl)
-        cont.make_public(ttl=args.ttl)
-    except:
-        print "ERROR: Could not create container", args.container
+    # Determine if the upload directory exists
+    if not os.path.isdir(args.directory):
+        print ("ERROR: Specified directory (%s) does not exist, please check "
+               "the path and try again)" % (args.directory))
         sys.exit(1)
-# Otherwise print the current CDN TTL associated with the existing container
-else:
-    print ("Container '%s' already exists - continuing with upload" %
-           (cont.name))
 
-# Set the static web index file (metadata/header)
-print "Setting static web index file to", args.index
-meta = {"X-Container-Meta-Web-Index": args.index}
-cf.set_container_metadata(cont, meta)
+    # Determine if CDN TTL is at least the minimum value permitted
+    if args.cdn_ttl < MIN_TTL:
+        print "ERROR: Minimum CDN TTL permitted is %ds" % (MIN_TTL)
+        sys.exit(2)
 
-# Check and confirm if the index file exists, otherwise create a placeholder
-# and inform/warn the client of the action
-index_file = args.directory + "/" + args.index
-if not os.path.isfile(index_file):
-    print "Index file '%s' not found, creating a placeholder" % (index_file)
-    f = open(index_file,'w')
-    f.write("Index page placeholder\n")
-    f.close()
+    # Same again for the CNAME record
+    if args.cname_ttl < DEFAULT_TTL:
+        print "ERROR: Minimum CNAME TTL permitted is %ds" % (DEFAULT_TTL)
+        sys.exit(3)
 
-# Grab a list of domains/zones
-(domains, count) = zone_list(dns)
+    # Determine if the FQDN is correctly formated (at least three segments
+    # separated by '.' are required).
+    #    NOTE: This can be improved since we're not checking whether or not
+    #          the zone in question is a valid TLD or if the string only has
+    #          valid (alphanumeric) characters
+    segments = args.fqdn.split('.')
+    if len(segments) < 3:
+        print ("ERROR: FQDN string is incorrectly formatted, please check "
+               "and try again")
+        print ("Base zone/domain in the format 'example.com' will not be "
+               "accepted")
+        sys.exit(4)
+    # All is apparently well, define the zone/domain using the FQDN string
+    else:
+        zone_name = '.'.join(segments[-(len(segments)-1):])
 
-# No zones found, request that one be created
-if count == 0:
-    print "You have no domains/zones at this time"
-    zone_name = raw_input("Please enter a zone name to create: ")
-    emailadd = raw_input("Please enter an e-mail contact for the zone: ")
-    ttl = get_ttl("NS")
-    # Try and create the new zone
+    # Define the authentication credentials file location and request that
+    # pyrax makes use of it. If not found, let the client/user know about it.
+
+    # Use a credential file in the following format:
+    # [rackspace_cloud]
+    # username = myusername
+    # api_key = 01234567890abcdef
+    # region = LON
+
     try:
-        zone = dns.create(name=zone_name, emailAddress=emailadd,
-                ttl=ttl, comment="Created through pyrax")
-    except e.DomainCreationFailed as err:
-        print "Zone creation failed:", err
-    print "Zone created:", zone.name
-# Zones found, enumerate and request client/user selects which one to use
-else:
-    print "Available zones/domains:"
-    for pos, zn in enumerate(domains):
-        print "%2d) %s" % (pos, zn.name)
-    # Keep requesting until a valid selection is made
-    zone = None
-    while not zone:
+        creds_file = os.path.expanduser(CONFIG_FILE)
+        pyrax.set_credential_file(creds_file, args.region)
+    except e.AuthenticationFailed:
+        print ("ERROR: Authentication failed. Please check and confirm "
+               "that the API username, key, and region are in place "
+               "and correct.")
+        sys.exit(5)
+    except e.FileNotFound:
+        print "ERROR: Credentials file '%s' not found" % (creds_file)
+        sys.exit(6)
+
+    # Use a shorter Cloud Files and Cloud DNS class reference strings
+    # This simplifies invocation later on (less typing)
+    cf = pyrax.cloudfiles
+    dns = pyrax.cloud_dns
+
+    # Grab zone list
+    domains = zone_list(dns)
+
+    # No zones found, inform that one needs to be created and exit
+    if len(domains) == 0:
+        print "ERROR: You have no domains/zones at this time"
+        print "Please create one first then try again"
+        sys.exit(7)
+
+    # Attempt to locate the zone extracted from FQDN string
+    try:
+        zone = [i for i in domains if zone_name in i.name][0]
+    except:
+        print "ERROR: Zone '%s' not found" % (zone_name)
+        print "Please check/create and try again"
+        sys.exit(8)
+
+    # Determine if container already exists, otherwise create it
+    try:
+        print "Checking if container already exists..."
+        cont = cf.get_container(args.container)
+    except e.NoSuchContainer:
+        cont = None
+
+    # Container not found, create it
+    if cont is None:
         try:
-            val = int(raw_input("Select a zone to add CNAME record to: "))
-            # User could potentially provide a negative integer, which will
-            # still be a valid index reference - including a crude catch here
-            if val < 0:
-                print "Invalid selection. Please try again."
-                zone = None
-            else:
-                zone = domains[val]
-        except IndexError:
-            print "Invalid selection. Please try again."
-        except ValueError:
-            print "Selection must be an integer listed, please try again."
+            print ("Container '%s' not found, creating with TTL set to %d..."
+                   % (args.container, args.cdn_ttl))
+            cont = cf.create_container(args.container)
+            cont.make_public(ttl=args.cdn_ttl)
+        except e.CDNFailed:
+            print "ERROR: Could not CDN enable the container"
+            sys.exit(9)
+        except:
+            print "ERROR: Could not create container", args.container
+            sys.exit(10)
 
-# We have determineded where the record needs to be added, request the
-# CNAME and TTL from the user/client
-prefix = None
-while not prefix:
-    prefix = raw_input("Please enter the prefix for the record (e.g. 'cdn'): ")
+    # Otherwise inform the user/client that the directory exists and
+    # determine if we can proceed (is the overwrite flag set)
+    else:
+        print ("Container '%s' found with TTL set to %d"
+               % (cont.name, cont.cdn_ttl))
+        if args.force:
+            print "Proceeding as upload has been forced"
+        else:
+            print "Force flag not set, exiting..."
+            sys.exit(11)
 
-cname = prefix + "." + zone.name
-ttl = get_ttl("CNAME")
-dest = cont.cdn_uri.replace("http://", "")
+    # Set the static web index file (metadata/header)
+    print "Setting static web index file to", args.index
+    meta = {"X-Container-Meta-Web-Index": args.index}
+    cf.set_container_metadata(cont, meta)
 
-# Attempt to create the new CNAME record
-cname_rec = {"type": "CNAME",
-        "name": cname,
-        "data": dest,
-        "ttl": ttl}
+    # Check and confirm if the index file exists, otherwise create a
+    # placeholder and inform/warn the client of the action
+    index_file = args.directory + "/" + args.index
+    if not os.path.isfile(index_file):
+        print ("Index file '%s' not found, creating a placeholder"
+               % (index_file))
+        f = open(index_file,'w')
+        f.write("Index page placeholder\n")
+        f.close()
 
-try:
-    rec = zone.add_record(cname_rec)
-    print ("-- Record details\n\tName: %s\n\tType: %s\n\tDestination: %s\n\t"
-           "TTL: %s") % (rec[0].name, rec[0].type, rec[0].data, rec[0].ttl)
-    print "Record added"
-except e.DomainRecordAdditionFailed as err:
-    print "Record creation failed:", err
+    # Start the upload
+    print "Beginning directory/folder upload"
+    (upload_key, total_bytes) = cf.upload_folder(args.directory, cont)
 
-# Start the upload
-print "Beginning directory/folder upload"
-(upload_key, total_bytes) = cf.upload_folder(args.directory, cont)
+    # Inform the user of the total upload size
+    print ("Total upload size: %d bytes (%.2f MB)"
+           % (total_bytes, total_bytes / 1024.0 / 1024))
 
-# Inform the user of the total upload size
-print ("Total upload size: %d bytes (%.2f MB)"
-       % (total_bytes, total_bytes / 1024.0 / 1024))
-
-# Prepare progress toolbar
-sys.stdout.write("[%s]" % (" " * toolbar_width))
-sys.stdout.flush()
-sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
-
-# Print the upload progress (1 second interval)
-uploaded = 0
-while uploaded < total_bytes:
-    uploaded = cf.get_uploaded(upload_key)
-    progress = int(ceil((uploaded * 100.0) / total_bytes / 2))
-    sys.stdout.write("%s" % ("=" * progress))
+    # Prepare progress toolbar
+    sys.stdout.write("[%s]" % (" " * TOOLBAR_WIDTH))
     sys.stdout.flush()
-    sys.stdout.write("\b" * (progress)) # Reset progress bar
-    sleep(1)
-print
+    # Return to start of line, after '['
+    sys.stdout.write("\b" * (TOOLBAR_WIDTH + 1))
 
-# Upload completed, confirm/print object count
-nms = cf.get_container_object_names(cont)
-print "Number of objects uploaded: %d" % (len(nms))
-print "Upload complete"
+    # Print the upload progress (1 second interval)
+    uploaded = 0
+    while uploaded < total_bytes:
+        uploaded = cf.get_uploaded(upload_key)
+        progress = int(ceil((uploaded * 100.0) / total_bytes / 2))
+        sys.stdout.write("%s" % ("=" * progress))
+        sys.stdout.flush()
+        sys.stdout.write("\b" * (progress)) # Reset progress bar
+        sleep(1)
+    print
+
+    # Upload completed, print object count and CDN URIs
+    objs = cf.get_container_object_names(cont)
+    print "Number of objects uploaded: %d" % (len(objs))
+
+    # Attempt to create the new CNAME record
+    cname_rec = {"type": "CNAME",
+            "name": args.fqdn,
+            "data": cont.cdn_uri.replace("http://", ""),
+            "ttl": args.cname_ttl}
+
+    try:
+        rec = zone.add_record(cname_rec)
+        print "Successfully added"
+        print ("-- Record details\n\tName: %s\n\tType: %s\n\tIP address: "
+               "%s\n\tTTL: %s") % (rec[0].name, rec[0].type, rec[0].data,
+               rec[0].ttl)
+    except e.DomainRecordAdditionFailed as err:
+        print "ERROR: Record addition request failed:", err
+        sys.exit(12)
+
+
+if __name__ == '__main__':
+    main()
